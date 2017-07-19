7c1314266803ce429bb57c7a12d7006cef09bd04
Genius <alex.rsilva18@gmail.com>
2017-07-19 11:01:32 -0500

    StatikA startup

--- StatikA/settings.py
+++ StatikA/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+#from dj_git submodule import submodule
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'testing',
+    'py-find-injection',
 ]
 
 MIDDLEWARE = [
--- StatikA/urls.py
+++ StatikA/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+from testing import urls as testing_urls
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    #url(r'^testing/', include(testing_urls, namespace='testing')),
+    url(r'^testing/', include(testing_urls, namespace='testing')),
     url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
 ]
